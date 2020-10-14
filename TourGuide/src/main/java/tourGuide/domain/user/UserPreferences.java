a3ce6a7ccfa57d8597538151ca9861239d55c2cb
olivier.bonheur <olivier.bonheur@gmail.com>
2020-10-14 15:22:53 +0200

    Some cleaning

--- TourGuide/src/main/java/tourGuide/domain/user/UserPreferences.java
+++ TourGuide/src/main/java/tourGuide/domain/user/UserPreferences.java
@@ -5,11 +5,6 @@ public class UserPreferences {
 	private int tripDuration = 1;
 	private int numberOfAdults = 1;
 	private int numberOfChildren = 0;
-	//private int attractionProximity = Integer.MAX_VALUE;
-	//private String currency = "USD";
-	//private Double lowerPricePoint = 0D;
-	//private Double highPricePoint = 1000000D;
-	//private int ticketQuantity = 1;
 
 	public UserPreferences() {
 	}
@@ -37,47 +32,5 @@ public class UserPreferences {
 	public void setNumberOfChildren(int numberOfChildren) {
 		this.numberOfChildren = numberOfChildren;
 	}
-/*
-	public void setAttractionProximity(int attractionProximity) {
-		this.attractionProximity = attractionProximity;
-	}
-	
-	public int getAttractionProximity() {
-		return attractionProximity;
-	}
 
-	// Added getter and setter for CurrencyUnit
-	public String getCurrency() {
-		return currency;
-	}
-
-	public void setCurrency(String currency) {
-		this.currency = currency;
-	}
-
-	public Double getLowerPricePoint() {
-		return lowerPricePoint;
-	}
-
-	public void setLowerPricePoint(Double lowerPricePoint) {
-		this.lowerPricePoint = lowerPricePoint;
-	}
-
-	public Double getHighPricePoint() {
-		return highPricePoint;
-	}
-
-	public void setHighPricePoint(Double highPricePoint) {
-		this.highPricePoint = highPricePoint;
-	}
-
-
-	public int getTicketQuantity() {
-		return ticketQuantity;
-	}
-
-	public void setTicketQuantity(int ticketQuantity) {
-		this.ticketQuantity = ticketQuantity;
-	}
-*/
 }
